76eec525e7ba89de845be8e8bb5cf6bcd0dd2e8a
rareaqil <rareaqil123@gmail.com>
2022-06-15 12:36:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,8 @@ def getResponse(ints, intents_json):
         if(i['tag'] == tag):
             result = random.choice(i['responses'])
             break
+        else:
+            result = "Maaf Kami Tidak Mengerti,Silahkan Coba Kata Kunci Lainnya."
     return result
 
 
